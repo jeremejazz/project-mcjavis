85b06ce0b316a618eee8601496c68e85bbc14780
rainserinas <raineirserinas@gmail.com>
2016-04-24 10:07:05 -0700

    update order page

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,6 +6,9 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use App\SaleInfo;
+use App\Menu;
+
 
 class AdminController extends Controller
 {
@@ -16,7 +19,49 @@ class AdminController extends Controller
 
     public function order()
     {
-        return view('administrator.order');
+        $result = new SaleInfo();
+        $test['sales_info'] = $result->fetchSaleInformations();
+
+        $response = [];
+
+
+        $index = 0;
+        foreach ($test['sales_info'] as $person) {
+            // get the menu order using sales_info_id
+            $Menu = new Menu();
+            //dump($person);
+
+            $response[$index]['sales_info'] = $person;
+
+
+            $response[$index]['menu_list'] = $result->fetchSaleInfoMenu(["" . $person->sale_information_id]);
+
+            // Get menu information
+            foreach ($response[$index]['menu_list'] as $menu_item) {
+
+                $flat_menu_info = $this->flatArray($Menu->getMenu(["" . $menu_item->menu_id]));
+                // Need to merge the value from other active records
+
+                //dump($flat_menu_info);
+                $response[$index]['menu_list_info'][] = (object)array_merge((array)$flat_menu_info, (array)$menu_item);
+            }
+
+            $index++;
+
+        }
+
+        $test = $response;
+
+        return view('administrator.order', compact('response'));
+    }
+
+    private function flatArray($array_to_flat)
+    {
+        foreach ($array_to_flat as $item) {
+            return $item;
+        }
+
+
     }
 
     public function showLoginForm()
--- resources/views/administrator/order.blade.php
+++ resources/views/administrator/order.blade.php
@@ -8,29 +8,37 @@
 </div>
 
 <div class="data-container">
-    <div id="1" class="col-lg-12" onclick=order_list($(this).attr('id'))>
+    <?php $counter = 1; ?>
+    @foreach( $response as $test )
+    <div id="{{$counter}}" class="col-lg-12" onclick=order_list($(this).attr('id'))>
         <div class="panel panel-primary">
             <div class="panel-heading">
 
+
                 <div class="row">
 
+
                     <div class="col-md-4">
-                        <p class="order-num">Order #: Dummy data</p>
+                        <p class="order-num">Order #: {{ "0000". $test['sales_info']->sale_information_id }}</p>
                     </div>
 
                     <div class="col-md-4">
-                        <p>Name: Dummy data</p>
-                        <p>Table #: Dummy data</p>
-                        <p>Senior Citizen:</p>
+                        <p>Name: {{ $test['sales_info']->first_name . " " . $test['sales_info']->last_name }} </p>
+                        <p>Table #: {{$test['sales_info']->table_no}}</p>
+                        <p>Senior Citizen: @if( $test['sales_info']->is_senior == 1 ) {{"Yes"}} @else {{"No"}}
+                            @endif</p>
                     </div>
                     <div class="col-md-4">
-                        <p>Order Type:</p>
-                        <p>Date/Time: Dummy data</p>
+                        <p>Order Type: @if( $test['sales_info']->is_take_out == 1 ) {{"Yes"}} @else {{"No"}} @endif</p>
+                        <p>Date/Time: {{$test['sales_info']->transaction_timestamp }}</p>
                     </div>
+
                 </div>
+
+
             </div>
             <div class="panel-body">
-                <div id="table-1" class="hid-table-data">
+                <div id="table-{{$counter}}" class="hid-table-data">
                     <table class="table table-hover table-data">
                         <thead>
                         <th>ITEMS</th>
@@ -39,12 +47,32 @@
                         <th>PRICE</th>
                         </thead>
                         <tbody>
+                        <!--<tr>
+                            <td>Test</td>
+                            <td>Test</td>
+                            <td>Test</td>
+                            <td>Test</td>
+                        </tr>-->
+                        <!-- LOOP THROUGH the order menu-->
+
+                        <?php $total_price = 0; ?>
+
+                        @foreach( $test['menu_list_info'] as $menu_info )
+
+                        <?php
+
+                        $total_price += $menu_info->price;
+
+                        ?>
                         <tr>
-                            <td>Test</td>
-                            <td>Test</td>
-                            <td>Test</td>
-                            <td>Test</td>
+                            <td> {{ $menu_info->description }}</td>
+                            <td>{{ $menu_info->quantity }}</td>
+                            <td>{{ $menu_info->variant }}</td>
+                            <td>{{ $menu_info->price }}</td>
                         </tr>
+
+                        @endforeach()
+
                         </tbody>
                     </table>
 
@@ -53,23 +81,28 @@
                     <div class="row table-details">
                         <div class="col-md-4">
                             <p>Payment:</p>
-                            <p>Change:</p>
-                            <p>Discount:</p>
+                            <p>Change: {{ $test['sales_info']->change }}</p>
+                            <p>Discount: @if( $test['sales_info']->is_senior == 1 ) {{"25%"}} @else {{"No"}} @endif </p>
                         </div>
                         <div class="col-md-4">
-                            <p>Payment Type:</p>
-                            <p>Payment Status:</p>
+                            <p>Payment Type: {{ "PayMaya"}}</p>
+                            <p>Payment Status: {{ $test['sales_info']->payment_status }}</p>
                         </div>
                         <div class="col-md-4">
                             <p>VAT:</p>
                             <p>Sub-total:</p>
-                            <p>Total:</p>
+                            <p>Total: {{ $total_price }}</p>
                         </div>
                     </div>
+
                 </div>
             </div>
+
+
         </div>
     </div>
+    <?php $counter++; ?>
+    @endforeach()
 </div>
 
 @endsection
