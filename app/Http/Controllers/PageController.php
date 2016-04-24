4255fbba3066e4521087c551860265ef433d8348
CipherShort <christopherdeuda@gmail.com>
2016-04-24 17:16:53 +0000

    Add pages controller for frontend

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class PageController extends Controller
+{
+    public function index(){
+        return view('pages.home');
+    }
+
+    public function cart(){
+        return view('pages.cart');
+    }
+
+    public function menu(){
+        return view('pages.menu');
+    }
+}
