b86594a3f6404399fbb09694e193f842de62a764
rainserinas <raineirserinas@gmail.com>
2016-04-23 22:09:04 -0700

    Add order panels

--- resources/views/administrator/administrator.blade.php
+++ resources/views/administrator/administrator.blade.php
@@ -2,10 +2,26 @@
 
 @section('app')
 
+<div class="container-tabs">
+
+    <ul class="nav nav-tabs">
+        <li class="active"><a data-toggle="tab" href="#home">Pending</a></li>
+        <li><a data-toggle="tab" href="#menu1">Done</a></li>
+    </ul>
+
+    <div class="tab-content">
+        <div id="home" class="tab-pane fade in active">
+            <h3>Pending List</h3>
+            <p>Some content.</p>
+        </div>
+        <div id="menu1" class="tab-pane fade">
+            <h3>Done List</h3>
+            <p>Some content.</p>
+        </div>
+    </div>
 
 
-        <p>Hello Hackathon</p>
-
+</div>
 
 
 @endsection
--- resources/views/administrator/order.blade.php
+++ resources/views/administrator/order.blade.php
@@ -4,28 +4,72 @@
 
 
 <div class="mytable-div">
-    <table class="myTable">
-        <thead>
-        <th>Order #</th>
-        <th>Table #</th>
-        <th>Name</th>
-        <th>Order List</th>
-        <th>Date/Time</th>
-        <th>Order Type</th>
-        <th>Senior Citizen</th>
-        </thead>
-        <tbody>
-        <tr>
-            <td>Test</td>
-            <td>Test</td>
-            <td>Test</td>
-            <td>Test</td>
-            <td>Test</td>
-            <td>Test</td>
-            <td>Test</td>
-        </tr>
-        </tbody>
-    </table>
+
+</div>
+
+<div class="data-container">
+    <div id="1" class="col-lg-12" onclick=order_list($(this).attr('id'))>
+        <div class="panel panel-primary">
+            <div class="panel-heading">
+
+                <div class="row">
+
+                    <div class="col-md-4">
+                        <p class="order-num">Order #: Dummy data</p>
+                    </div>
+
+                    <div class="col-md-4">
+                        <p>Name: Dummy data</p>
+                        <p>Table #: Dummy data</p>
+                        <p>Senior Citizen:</p>
+                    </div>
+                    <div class="col-md-4">
+                        <p>Order Type:</p>
+                        <p>Date/Time: Dummy data</p>
+                    </div>
+                </div>
+            </div>
+            <div class="panel-body">
+                <div id="table-1" class="hid-table-data">
+                    <table class="table table-hover table-data">
+                        <thead>
+                        <th>ITEMS</th>
+                        <th>QTY</th>
+                        <th>VARIANT</th>
+                        <th>PRICE</th>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>Test</td>
+                            <td>Test</td>
+                            <td>Test</td>
+                            <td>Test</td>
+                        </tr>
+                        </tbody>
+                    </table>
+
+                    <hr>
+
+                    <div class="row table-details">
+                        <div class="col-md-4">
+                            <p>Payment:</p>
+                            <p>Change:</p>
+                            <p>Discount:</p>
+                        </div>
+                        <div class="col-md-4">
+                            <p>Payment Type:</p>
+                            <p>Payment Status:</p>
+                        </div>
+                        <div class="col-md-4">
+                            <p>VAT:</p>
+                            <p>Sub-total:</p>
+                            <p>Total:</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 @endsection
